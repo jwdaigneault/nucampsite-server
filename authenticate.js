cbb449cf9a31ed3c5980a361a40ad0b2abe3f975
Jessie Daigneault <jwdaigneault15@gmail.com>
2021-04-25 14:40:42 -0400

    Week 3 Workshop

--- authenticate.js
+++ authenticate.js
@@ -37,4 +37,14 @@ exports.jwtPassport = passport.use(
     )
 );
 
+exports.verifyAdmin = function(req, res, next) {
+    if (req.user.admin) {
+        return next();
+    } else {
+        res.statusCode = 403;
+        err = new Error("You are not authorized to perform this operation!");
+        return next(err);
+    }
+}
+
 exports.verifyUser = passport.authenticate('jwt', {session: false});
